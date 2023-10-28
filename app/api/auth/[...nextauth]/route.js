17aacb3d473e490d6165ece50482c0cf3df65c9a
zana-abdollahpour <zana.ap4@gmail.com>
2023-10-28 14:28:12 +0330

    add API route and provider

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google";
+import GitHubProvider from "next-auth/providers/github";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {},
+});
+
+export { handler as GET, handler as POST };
