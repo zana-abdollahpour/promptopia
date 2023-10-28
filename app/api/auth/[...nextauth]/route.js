26a0ae647f26bace9a15f49f7d8111688699b8ea
zana-abdollahpour <zana.ap4@gmail.com>
2023-10-28 14:53:21 +0330

    finish auth route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,6 +2,9 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
 
+import { connectToDB } from "@utils/database";
+import User from "@models/User";
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -13,8 +16,36 @@ const handler = NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
-  async session({ session }) {},
-  async signIn({ profile }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
+
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
+
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
 });
 
 export { handler as GET, handler as POST };
