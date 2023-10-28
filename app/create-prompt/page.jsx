705c445a0024cd040565898aeaaa2bd4cfbd4e5f
zana-abdollahpour <zana.ap4@gmail.com>
2023-10-28 22:15:02 +0330

    implement Feed section

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
